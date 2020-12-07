80a3e0881e841fc6bb5b292759135325ee567e94
Ivan Bober <ivibober@gmail.com>
2020-12-07 11:53:30 -0300

    Initial Commit

---
+++ RedditProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedditProject
+//
+//  Created by Ivan Bober on 07/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
